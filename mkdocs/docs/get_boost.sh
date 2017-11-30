790445da852a8d7e2ba6019c9e2c531d0672bc21
adam <aryczkowski@imgw.pl>
2017-11-30 18:53:16 +0100

    Added a script that downloads boost

---
+++ mkdocs/docs/get_boost.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.bz2
+tar xjf boost_1_61_0.tar.bz2
+cd boost_1_61_0/tools/build
+./bootstrap.sh
+./b2 install --prefix=$HOME
+cd ../..
+b2 --build-dir=/tmp toolset=gcc stage  #gcc for gcc toolchain
+
+
+#Now we tell Cmake about the location of the boost libraries
